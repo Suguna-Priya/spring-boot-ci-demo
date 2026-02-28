52e4a5b3d66739814019dbc2890f83e8072d356a
Suguna-Priya <sugunasampath007@gmail.com>
2026-03-01 00:50:31 +0530

    pushing all the code

---
+++ src/main/java/com/example/ci_demo/CiDemoController.java
@@ -0,0 +1,13 @@
+package com.example.ci_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CiDemoController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello from CI/CD Pipeline";
+    }
+}
